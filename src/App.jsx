9c47bd74253b729ab93ddf5fd1b99b98bad105c3
Michael Matheus <michaelmatheus2001@gmail.com>
2023-08-13 12:21:26 -0300

    build: import Sidebar component, Post Componet, App CSS Module & add wrapper element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { Header } from './components/Header'
+import { Sidebar } from './components/Siderbar'
+import { Post } from './components/Post'
+
+import styles from './App.module.css'
 
 import './global.css'
 
 
 export default function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+
+          <Post author="Michael Matheus" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel incidunt, perspiciatis error rem voluptatem commodi ad amet accusamus doloremque. Perspiciatis exercitationem ipsa culpa doloribus blanditiis vel ut eligendi cum accusamus?" />
+          <Post author="Bianca Marques" content="Um post muito legal!" />
+
+        </main>
+      </div>
+    </div>
   )
 }
