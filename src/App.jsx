87939b3d9dc59e61bbe8027d9374df6d60e20281
Michael Matheus <michaelmatheus2001@gmail.com>
2023-08-14 06:20:26 -0300

    fix: change sidebar path wrong name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from './components/Header'
-import { Sidebar } from './components/Siderbar'
+import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
 import styles from './App.module.css'
