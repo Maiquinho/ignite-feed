3bf420b093d819fc6e64d299a3e6ffd8ce7282d2
Michael Matheus <michaelmatheus2001@gmail.com>
2023-08-18 05:41:27 -0300

    build: add Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+
+
+export function Comment() {
+
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/Maiquinho.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Michael Matheus</strong>
+                            <time dateTime="2023-08-16 05:00:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
