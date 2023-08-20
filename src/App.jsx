83007fd58970399e451e05fdb016501d673af608
Michael Matheus <michaelmatheus2001@gmail.com>
2023-08-20 17:00:56 -0300

    build: add posts static data for component iteration

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,36 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/maiquinho.png',
+      name: 'Michael Matheus',
+      role: 'Full-stack Developer @Publinet'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-20 16:50:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-08-19 16:50:00'),
+  }
+];
 
 export default function App() {
   return (
@@ -16,8 +46,14 @@ export default function App() {
         <Sidebar />
         <main>
 
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
 
         </main>
       </div>
