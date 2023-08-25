cd860e8a40632c0140b2d8941c95773155d2670e
Michael Matheus <michaelmatheus2001@gmail.com>
2023-08-25 10:36:48 -0300

    build: add handle delete comment function using child to parent prop communication

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,11 @@ import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
 
     return (
         <div className={styles.comment}>
@@ -17,7 +21,7 @@ export function Comment({ content }) {
                             <time dateTime="2023-08-16 05:00:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button title="Deletar comentário" onClick={handleDeleteComment}>
                             <Trash size={24} />
                         </button>
                     </header>
